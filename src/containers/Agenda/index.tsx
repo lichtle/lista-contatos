822e5ba97f18f5de724ed51cea04ac36df9c127c
“lichtle” <leticia.negreiros@outlook.com>
2024-01-23 21:51:24 -0300

    chore: criação do container Agenda

---
+++ src/containers/Agenda/index.tsx
@@ -0,0 +1,15 @@
+const Agenda = () => (
+  <main>
+    <h1>Contatos</h1>
+    <div>
+      <span>Nome completo: xxx</span>
+      <span>E-mail: xxx</span>
+      <span>Telefone: xxx</span>
+    </div>
+    <div>
+      <span>Nome completo: xxx</span>
+      <span>E-mail: xxx</span>
+      <span>Telefone: xxx</span>
+    </div>
+  </main>
+);
---
+++ src/containers/Main/index.tsx
@@ -0,0 +1,43 @@
+const Agenda = () => (
+  <main>
+    <h1>Contatos</h1>
+
+    <div>
+      {/* Contato */}
+      <div>
+        {/* Dados */}
+        <span>Nome completo: xxx</span> {/*  Dado */}
+        <span>E-mail: xxx</span>
+        <span>Telefone: xxx</span>
+      </div>
+      <div>
+        {/* Ações */}
+        <button>Editar</button>
+        {/* Botões */}
+        <button>Excluir</button>
+      </div>
+    </div>
+
+    <div>
+      {/* Contato */}
+      <div>
+        {/* Dados */}
+        <span>Nome completo: xxx</span> {/*  Dado */}
+        <span>E-mail: xxx</span>
+        <span>Telefone: xxx</span>
+      </div>
+      <div>
+        {/* Ações */}
+        <button>Editar</button>
+        {/* Botões */}
+        <button>Excluir</button>
+      </div>
+    </div>
+
+    <div>
+      <button>Adicionar um contato</button>
+    </div>
+  </main>
+);
+
+export default Agenda;
