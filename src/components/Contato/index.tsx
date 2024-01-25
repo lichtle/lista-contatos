b344b547f078d7511bb967f8e50d402d22fff75a
“lichtle” <leticia.negreiros@outlook.com>
2024-01-25 15:25:43 -0300

    feat: adicionando as funções de 'adicionar' e 'remover' um contato

--- src/components/Contato/index.tsx
+++ src/components/Contato/index.tsx
@@ -1,22 +1,39 @@
-import * as S from "./styles";
-import Acoes from "../Acoes";
+import { useDispatch } from "react-redux";
 
-const Contato = () => (
-  <S.Dados>
-    {/* Dados */}
-    <div>
-      <span>
-        <span>Nome completo:</span> Leticia Fernandes Negreiros
-      </span>
-      <span>
-        <span>E-mail:</span> example@mail.com
-      </span>
-      <span>
-        <span>Telefone:</span> 00000000000
-      </span>
-    </div>
-    <Acoes />
-  </S.Dados>
-);
+import * as S from "./styles";
+import ContatoClass from "../../models/Contato";
+import { remover } from "../../redux/contact/slice";
+import { BotaoEditar, BotaoExcluir } from "./styles";
+
+type Props = ContatoClass; // Renomeamos a classe 'Tarefa' para não dar conflito com o nome da classe criada abaixo, que já é 'Tarefa'
+
+const Contato = ({ nome, email, telefone }: Props) => {
+  const dispatch = useDispatch();
+
+  return (
+    <S.Dados>
+      <div>
+        <span>
+          <span>Nome completo: </span>
+          {nome}
+        </span>
+        <span>
+          <span>E-mail: </span>
+          {email}
+        </span>
+        <span>
+          <span>Telefone: </span>
+          {telefone}
+        </span>
+      </div>
+      <div>
+        <BotaoEditar>Editar</BotaoEditar>
+        <BotaoExcluir onClick={() => dispatch(remover(telefone))}>
+          Excluir
+        </BotaoExcluir>
+      </div>
+    </S.Dados>
+  );
+};
 
 export default Contato;
--- src/containers/Agenda/index.tsx
+++ src/containers/Agenda/index.tsx
@@ -1,15 +1,31 @@
-import { AgendaContainer, BotaoAdicionar } from "./styles";
-import Acoes from "../../components/Acoes";
+import { useSelector } from "react-redux";
+
+import { RootReducer } from "../../redux/store";
+import { AgendaContainer, BotaoAdicionarVoltar } from "./styles";
 import Contato from "../../components/Contato";
 
-const Agenda = () => (
-  <AgendaContainer>
-    <h1>Contatos</h1>
-    <BotaoAdicionar>Adicionar um contato</BotaoAdicionar>
-    <Contato />
-    <Contato />
-    <Contato />
-  </AgendaContainer>
-);
+const Agenda = () => {
+  const { contatos } = useSelector((state: RootReducer) => state.contatos);
+
+  return (
+    <AgendaContainer>
+      <h1>Contatos</h1>
+      <BotaoAdicionarVoltar to="/adicionar">
+        Adicionar um contato
+      </BotaoAdicionarVoltar>
+      <ul>
+        {contatos.map((contato) => (
+          <li key={contato.telefone}>
+            <Contato
+              nome={contato.nome}
+              email={contato.email}
+              telefone={contato.telefone}
+            />
+          </li>
+        ))}
+      </ul>
+    </AgendaContainer>
+  );
+};
 
 export default Agenda;
